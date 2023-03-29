2ccf687c6dcd1a712e71c15dd50f05c773d55b90
PabloADB <>
2023-03-29 13:57:14 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php
+       $nombre_completo = "Pablo Antonio Dominguez Bustos";
+       $calle = "Calle de la Rosa";
+       $numero_casa = "123";
+       $colonia = "La Condesa";
+       $municipio = "Ciudad de México";
+       $telefono = "555-123-4567";
+       echo "Nombre completo: " . $nombre_completo . "<br>";
+       echo "Dirección: " . $calle . " " . $numero_casa . ", " . $colonia . ", " . $municipio . "<br>";
+       echo "Teléfono: " . $telefono;
+    ?>
+
+  </body>
+</html>
